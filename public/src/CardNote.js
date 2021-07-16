bcc7408f602e6504e6cd2a8bbad9c8639af47d92
devanshukushwah <61180182+devanshukushwah@users.noreply.github.com>
2021-07-17 03:41:22 +0530

    first message

--- public/src/CardNote.js
+++ public/src/CardNote.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react"
 import Container from "./components/Container/Container"
-// import ContextMenu from "./components/ContextMenu/ContextMenu"
 import DirectoryBar from "./components/DirectoryBar/DirectoryBar"
 import MenuBar from "./components/MenuBar/MenuBar"
 import Modal from "./components/Modal/Modal"
